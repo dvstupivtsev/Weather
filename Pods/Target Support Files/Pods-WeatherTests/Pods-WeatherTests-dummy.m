ce196e7692a2a51994f6600b9894af9e9b478026
dvstupivtsev <dvstupivtsev@gmail.com>
2019-06-09 22:52:46 +0300

    remove cocoapods

--- Pods/Target Support Files/Pods-WeatherTests/Pods-WeatherTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WeatherTests : NSObject
-@end
-@implementation PodsDummy_Pods_WeatherTests
-@end
--- Pods/Target Support Files/Weakify/Weakify-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Weakify : NSObject
-@end
-@implementation PodsDummy_Weakify
-@end
